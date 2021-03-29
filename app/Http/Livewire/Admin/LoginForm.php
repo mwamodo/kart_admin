465099e0bc0bc69631b190a31f3325689620fd5f
Malik Mwamodo <erick@mwamodo.com>
2021-03-29 12:02:58 +0300

    WIP

--- app/Http/Livewire/Admin/LoginForm.php
+++ app/Http/Livewire/Admin/LoginForm.php
@@ -4,10 +4,10 @@ namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
 
-class Login extends Component
+class LoginForm extends Component
 {
     public function render()
     {
-        return view('livewire.admin.login');
+        return view('livewire.admin.login-form');
     }
 }
